0c55e54e3b2ff1aaddbeaa8f6c1cca927528e397
Fabian Telschow <ftelschow@ucsd.edu>
2021-01-12 10:22:55 +0100

    changed code according to style guidelines

--- RFT/R/GaussianKinematicFormulas.R
+++ RFT/R/GaussianKinematicFormulas.R
@@ -10,99 +10,137 @@
 #
 #------------------------------------------------------------------------------#
 # Developer notes:
-# - Style guideline NOT included
+# - Style guideline included
+# - specify df for the different fields in the description
 #------------------------------------------------------------------------------#
 
 #' Computes the d-th EC density of different fields. Currently only "t" is implemented.
 #'
 #' @param x Numeric location x at which the EC density is to be evaluated
-#' @param d Numeric d-th EX density
-#' @param type String type of field for EC densities, currently only "t" (Student-t) and "z" (Gaussian) are supported
-#' @param df Numeric degree of freedom of the field. For "t" this is a numeric.
-#' @return value of 2d-EC density of a t-field of degree df at location x.
+#' @param d Numeric d-th EC density
+#' @param field String type of field for EC densities, currently only
+#' "z" (Gaussian), "t" (Student-t) and "chi2" are supported
+#' @param df Numeric degrees of freedom of the field.
+#' @return value of EC density of the chosen field x.
 #' @export
-EC_density <- function( x, d, type = "t", df = 1 ){
-  # switch by type of the marginal distribution of the field
-  if( type == "t" ){
-    # compute the density depending on the dimension
+ECdensity <- function( x,
+                       d,
+                       field = "t",
+                       df    = 1 ){
+  if( field == "t" ){
+    # Compute the EC densities for a t-field up to D = 3
     if( d == 0 ){
       1 - pt( x, df = df )
+
     }else if( d == 1 ){
       ( 2*pi )^(-1) * ( 1 + x^2 / df )^( -( df - 1 ) / 2 )
+
     }else if( d == 2 ){
-      ( 2*pi )^( -3 / 2 ) * gamma( ( df + 1 ) / 2 ) / gamma( df / 2 ) /
-      sqrt( df ) * sqrt(2) * x * ( 1 + x^2 / df )^( -( df - 1 ) / 2 )
+      (2*pi)^(-3/2) * gamma( ( df + 1 ) / 2 ) / gamma( df / 2 ) / sqrt(df) *
+        sqrt(2) * x * ( 1 + x^2 / df )^( -( df - 1 ) / 2 )
+
     }else if( d == 3 ){
-      ( 2*pi )^( -2 ) * ( ( df - 1 ) / df * x^2 - 1 ) *
-      ( 1 + x^2 / df )^( -( df - 1 ) / 2 )
+      (2*pi)^(-2) * ( ( df - 1 ) / df * x^2 - 1 ) *
+        ( 1 + x^2 / df )^( -( df - 1 ) / 2 )
+
     }else{
-      stop("Error: d must be smaller then 3. Higher dimensions are not yet implemented.")
+      stop( "Error: d must be smaller then 3. Higher dimensions
+               are not yet implemented." )
     }
 
-  }else if( type == "z" ){
-    # function appearing in all densities
-    constFunc = ( 2*pi )^( -( d + 1 ) / 2 ) * exp( -x^2 / 2 )
+  }else if( field == "z" ){
+    # Constant factor appearing in all EC densities
+    constFunc = ( 2 * pi )^( -( d + 1 ) / 2 ) * exp( -x^2 / 2 )
 
-    # compute the density depending on the dimension
+    # Compute the EC densities for a z-field (gaussian) up to D = 3
     if( d == 0 ){
       1 - pnorm( x )
+
     }else if( d == 1 ){
       constFunc * 1
+
     }else if( d == 2 ){
-      constFunc * ( 2 * x )
+      constFunc * 2 * x
+
     }else if( d == 3 ){
       constFunc * ( 4 * x^2 - 2 )
+
     }else{
-      stop("Error: d must be smaller then 4. Higher dimensions are not yet implemented.")
+      stop( "Error: d must be smaller then 4. Higher dimensions
+               are not yet implemented." )
     }
+
+  }else if( field == "chi2" ){
+    # Compute the EC densities for a ch2-field up to D = 1
+    if( d == 0 ){
+      ifelse( x >= 0,
+              pchisq( x, df = df, lower.tail = FALSE ),
+              0 )
+
+    }else if( d == 1 ){
+      ifelse( x >= 0,
+              x^( ( df - 1 ) / 2 ) * exp( -x / 2 ) / sqrt( 2 * pi ) /
+                2^( df / 2 - 1 ) / gamma( df / 2 ),
+              0 )
+
+    }else{
+      stop( "Error: d must be smaller then 4. Higher dimensions are
+             not yet implemented." )
+    }
+
   }else{
-    stop("Error: Input a valid field type.")
+    stop( "Error: Input a valid field type." )
   }
 }
 
-#' Approximates the upper tail probabilities of different fields using the corresponding Gaussian
-#' kinematic formulas.
-#' It approximates the tail probability of the absolute value by twice the
-#' usual tail probability of the process.
+#' Returns the expected Euler characterisitc curve for a specified field and its
+#' Lipshitz-Killing-curvatures
 #'
 #' @param LKC Vector estimated LKCs of the UGRFs of the field.
-#' @param type String type of field.
-#' @param df degrees of freedom.
+#' @param field String type of field for EC densities, currently
+#' "z" (Gaussian), "t" (Student-t) and "chi2" are supported. Default is "z".
+#' @param df Numeric degrees of freedom of the field.
 #' @return a function computing the EEC curve
 #' @export
 EEC <- function( LKC,
-                 type  = "t",
+                 field  = "z",
                  df    = 0
                  ){
 
-  ###### Approximate the tail distribution using the GKF and EECH
-  if( length( LKC ) == 2 ){
-    ### 1D case
-    EECf <- function(u){
-              LKC[1] * EC_density( x = u, d = 0,
-                                   type = type, df = df ) +
-              LKC[2] * EC_density( x = u, d = 1, type = type, df = df )
+  #----- Approximate the tail distribution using the GKF and EECH
+  if( length(LKC) == 2 ){
+    # 1D case
+    EECf <- function( u ){
+              LKC[1] * EC_density( x    = u,
+                                   d    = 0,
+                                   field = field,
+                                   df   = df ) +
+              LKC[2] * EC_density( x    = u,
+                                   d    = 1,
+                                   field = field,
+                                   df   = df )
     }
 
   }else if( length(LKC) == 3 ){
-    ### 2D case
+    # 2D case
     EECf <- function(u){
-              LKC[1] * EC_density( x = u, d = 0, type = type, df = df ) +
-              LKC[2] * EC_density( x = u, d = 1, type = type, df = df ) +
-              LKC[3] * EC_density( x = u, d = 2, type = type, df = df )
+              LKC[1] * EC_density( x = u, d = 0, field = field, df = df ) +
+              LKC[2] * EC_density( x = u, d = 1, field = field, df = df ) +
+              LKC[3] * EC_density( x = u, d = 2, field = field, df = df )
     }
 
   }else if( length(LKC) == 4 ){
-    ### 3D case
+    # 3D case
     EECf <- function(u){
-              LKC[1] * EC_density( x = u, d = 0, type = type, df = df ) +
-              LKC[2] * EC_density( x = u, d = 1, type = type, df = df ) +
-              LKC[3] * EC_density( x = u, d = 2, type = type, df = df ) +
-              LKC[4] * EC_density( x = u, d = 3, type = type, df = df )
+              LKC[1] * EC_density( x = u, d = 0, field = field, df = df ) +
+              LKC[2] * EC_density( x = u, d = 1, field = field, df = df ) +
+              LKC[3] * EC_density( x = u, d = 2, field = field, df = df ) +
+              LKC[4] * EC_density( x = u, d = 3, field = field, df = df )
     }
 
   }else{
-    stop( "Error: Not yet implemented for data with a domain of dimension greater than 3!")
+    stop( "Error: Not yet implemented for data with a domain of dimension
+           greater than 3!" )
   }
 
   Vectorize( EECf )
@@ -111,25 +149,29 @@ EEC <- function( LKC,
 #' Computes a threshold of the EEC curve. This can be used to control the FWER of
 #' random fields.
 #'
-#' @param alpha Numeric upper tail probabiltiy, which needs to be approximated.
 #' @param LKC Vector estimated LKCs of the UGRFs of the field.
-#' @param type String type of field.
-#' @param df degrees of freedom.
+#' @param alpha Numeric upper tail probabiltiy, which needs to be approximated.
+#' @param field String type of field for EC densities, currently
+#' "z" (Gaussian), "t" (Student-t) and "chi2" are supported. Default is "z".
+#' @param df Numeric degrees of freedom of the field.
+#' @param interval Vector with two components containing the upper and lower
+#' bound for finding the root of the EEC curve. Default c(0,50).
 #' @return Numeric the approximated 1-alpha quantile.
 #' @export
 EEC_threshold <- function( LKC,
                            alpha    = 0.05,
-                           type     = "t",
+                           field    = "t",
                            df       = 0,
                            interval = c( 0, 50 )
                            ){
-
-  EEC_function <- EEC( LKC, type = type, df )
+  # Get the EEC curve and subtract alpha
+  EEC_function <- EEC( LKC, field = field, df )
   tailProb     <- function( u ){ EEC_function( u ) - alpha }
 
+  # return the results
   list( threshold = uniroot( tailProb, interval )$root,
         EEC   = EEC_function,
         alpha = alpha,
-        type  = type
+        field = field
   )
 }
